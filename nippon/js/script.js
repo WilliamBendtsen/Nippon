4b1fcf521468fdbf66ab2f1ded8c8c11ec8bb62c
WilliamBendtsen <143700870+WilliamBendtsen@users.noreply.github.com>
2024-02-26 13:11:49 +0100

    Add files via upload

---
+++ nippon/js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollDownLinks = document.querySelectorAll('.scroll-down-link');
+    scrollDownLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
